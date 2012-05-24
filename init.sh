2bc08df22f06f67008ef6f9662909bdc2db45b6d
Eugene Van den Bulke <eugene.vandenbulke@gmail.com>
2012-05-24 16:29:27 +1000

    auto install vim vundle and plugins

--- init.sh
+++ init.sh
@@ -1,1 +1,11 @@
-ln -s ~/Code/dotfiles/vimrc ~/.vimrc
+if ! [ -L "$HOME/.vimrc" ]; then
+    rm -f ~/.vimrc
+    ln -s ~/Code/dotfiles/vimrc ~/.vimrc
+fi
+
+if ! [ -d "$HOME/.vim/bundle/vundle" ]; then
+    git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+    vim +BundleInstall +qall
+fi
+
+
